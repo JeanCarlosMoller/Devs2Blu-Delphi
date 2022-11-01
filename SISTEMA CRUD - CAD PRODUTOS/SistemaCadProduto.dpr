aede1a0b9ecb5bca63250c64e2037b63b7656f71
FURB\jcmoller <jeancmoller17@gmail.com>
2022-10-31 22:00:16 -0300

    criando conexão com o banco de dados

--- SISTEMA CRUD - CAD PRODUTOS/SistemaCadProduto.dpr
+++ SISTEMA CRUD - CAD PRODUTOS/SistemaCadProduto.dpr
@@ -2,13 +2,16 @@ program SistemaCadProduto;
 
 uses
   Vcl.Forms,
-  uFrmPrincipal in 'uFrmPrincipal.pas' {Form1};
+  uFrmPrincipal in 'uFrmPrincipal.pas' {Form1},
+  UdmPedidos in 'UdmPedidos.pas' {dmPedidos: TDataModule},
+  UfrmCompradores in 'UfrmCompradores.pas' {frmCompradores};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TdmPedidos, dmPedidos);
   Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
